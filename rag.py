0af81eaa2807a9743dd9e647f7c1500cb8613652
Freddy Khant <freddyhmk@gmail.com>
2025-01-02 15:14:00 +0800

    self correction

--- rag.py
+++ rag.py
@@ -84,15 +84,118 @@ doc_grader_prompt = """ Here is the retrieved document. \n\n {document} \n\n Her
 
 Please carefully and objectively assess whether the document contains at least some information that is relevant to the question.
 
-Return JSON with ONLY single key, binary_score, that is 'yes' or 'no' score to indicate whether the document contains at least some relevant information to the question."""
+Return JSON with ONLY single key - binary_score, that is either 'yes' or 'no' score to indicate whether the document contains at least some relevant information to the question."""
 
 # test retrieval grader
 question = "What is the total sales for Coffee Heaven?"
 docs = retriever.invoke(question)
 doc_txt = docs[1].page_content
-doc_grader_prompt_formatted = doc_grader_prompt.format(document=doc_txt, question=question)
+doc_grader_prompt_formatted = doc_grader_prompt.format(
+    document=doc_txt, question=question
+)
 result = llm_json_mode.invoke(
-  [SystemMessage(content=doc_grader_instructions)]
-  + [HumanMessage(content=doc_grader_prompt_formatted)]
+    [SystemMessage(content=doc_grader_instructions)]
+    + [HumanMessage(content=doc_grader_prompt_formatted)]
 )
 json.loads(result.content)
+
+### answer generator ###
+
+# prompt
+rag_prompt = """You are an assistant for question-answering tasks. 
+
+Here is the context to use to answer the question:
+
+{context} 
+
+Think carefully about the above context. 
+
+Now, review the user question:
+
+{question}
+
+Provide an answer to these questions using only the above context. 
+
+Use three sentences maximum and keep the answer concise.
+
+Answer:"""
+
+# post processing
+def format_docs(docs):
+  return "\n\n".join([doc.page_content for doc in docs])
+
+# test generation
+docs = retriever.invoke(question)
+docs_txt = format_docs(docs)
+rag_prompt_formatted = rag_prompt.format(context=docs_txt, question=question)
+generation = llm.invoke([HumanMessage(content=rag_prompt_formatted)])
+print("\n")
+print(generation)
+
+### hallucination grader ###
+
+# Hallucination Grader Instructions
+hallucination_grader_instructions = """
+
+You are a teacher grading a quiz.
+
+You will be given FACTS and a STUDENT ANSWER.
+
+Here is the grade criteria to follow:
+
+(1) Ensure the STUDENT ANSWER is grounded in the FACTS.
+
+(2) Ensure the STUDENT ANSWER does not contain "hallucinated" information outside of the scope of the FACTS.
+
+Score:
+
+A score of yes means that the student's answer meets all of the criteria. This is the highest (best) score. 
+
+A score of no means that the student's answer does not meet all of the criteria. This is the lowest possible score you can give.
+
+Explain your reasoning in a step-by-step manner to ensure your reasoning and conclusion are correct. 
+
+Avoid simply stating the correct answer at the outset."""
+
+# Hallucination Grader Prompt
+hallucination_grader_prompt = """FACTS: \n\n {documents} \n\n STUDENT ANSWER: {generation}. 
+
+Return JSON with ONLY two two keys, binary_score is 'yes' or 'no' score to indicate whether the STUDENT ANSWER is grounded in the FACTS. And a key, explanation, that contains an explanation of the score."""
+
+# test hallucination grader
+
+hallucination_grader_prompt_formatted = hallucination_grader_prompt.format(
+    documents=docs_txt, generation=generation.content
+)
+result = llm_json_mode.invoke(
+   [SystemMessage(content=hallucination_grader_instructions)] 
+   + [HumanMessage(content=hallucination_grader_prompt_formatted)]
+)
+json.loads(result.content)
+
+### answer grader ###
+
+answer_grader_instructions = """You are a teacher grading a quiz. 
+
+You will be given a QUESTION and a STUDENT ANSWER. 
+
+Here is the grade criteria to follow:
+
+(1) The STUDENT ANSWER helps to answer the QUESTION
+
+Score:
+
+A score of yes means that the student's answer meets all of the criteria. This is the highest (best) score. 
+
+The student can receive a score of yes if the answer contains extra information that is not explicitly asked for in the question.
+
+A score of no means that the student's answer does not meet all of the criteria. This is the lowest possible score you can give.
+
+Explain your reasoning in a step-by-step manner to ensure your reasoning and conclusion are correct. 
+
+Avoid simply stating the correct answer at the outset."""
+
+# Grader prompt
+answer_grader_prompt = """QUESTION: \n\n {question} \n\n STUDENT ANSWER: {generation}. 
+
+Return JSON with two keys, binary_score is 'yes' or 'no' score to indicate whether the STUDENT ANSWER meets the criteria. And a key, explanation, that contains an explanation of the score."""
